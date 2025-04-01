cd828e4214e0cb9657e1f871b61b1f244ae9c0cd
vianAja <najwanoctavian@gmail.com>
2025-04-01 19:40:10 +0700

    "test commit manual"

---
+++ run_push_gitlab.py
@@ -0,0 +1,35 @@
+import subprocess
+from dotenv import load_dotenv
+import os
+
+load_dotenv('.creds/.env')
+
+## Credentials for GitLab project
+username_gitlab = os.getenv('GITLAB_USERNAME')
+password_gitlab = os.getenv('GITLAB_PASSWORD')
+
+
+def push_gitlab(commit_name: str,):
+    subprocess.run([
+        'git',
+        'add',
+        '.'
+    ], text=False)
+    subprocess.run([
+        'git','commit','-m',f'"{commit_name}"'
+    ], text=False)
+    proses_push = subprocess.Popen(
+        [
+            'git','push','origin','main'
+        ],
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True
+    )
+
+    proses_push.stdin.write(f'{username_gitlab}\n{password_gitlab}')
+    proses_push.stdin.close()
+
+if __name__ == '__main__':
+    push_gitlab(commit_name='test commit manual')
